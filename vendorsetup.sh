6b1468b24d58dec37159033a1d73bcbe51dc1109
GuaiYiHu <guaiyihu@foxmail.com>
2017-08-27 17:37:40 +0800

    leo: add patch for sensor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
 add_lunch_combo lineage_leo-eng
 add_lunch_combo lineage_leo-userdebug
 
+applyminotepropatch() {
+	sh device/xiaomi/leo/patch/applypatch.sh
+}
+
+echo "** Make sure you have run after repo sync: applyminotepropatch"
