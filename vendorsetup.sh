0f5847bd57c89238322e7d1457a12bec71ef95e3
GuaiYiHu <guaiyihu@foxmail.com>
2017-09-01 18:53:51 +0800

    Revert "leo: add patch for sensor"
    This reverts commit 6b1468b24d58dec37159033a1d73bcbe51dc1109.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,3 @@
 add_lunch_combo lineage_leo-eng
 add_lunch_combo lineage_leo-userdebug
 
-applyminotepropatch() {
-	sh device/xiaomi/leo/patch/applypatch.sh
-}
-
-echo "** Make sure you have run after repo sync: applyminotepropatch"
